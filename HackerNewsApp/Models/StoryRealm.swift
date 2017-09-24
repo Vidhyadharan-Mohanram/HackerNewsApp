5149018bc9c50a62c576a419c37e068129a76b3b
Vidhyadharan Mohanram <Vidhya_dharan@iCloud.com>
2017-09-24 23:56:23 +0530

    Removed unwanted files and adding missed files.

--- HackerNewsApp/Models/StoryRealm.swift
+++ HackerNewsApp/Models/StoryRealm.swift
@@ -5,60 +5,79 @@
 //	Copyright © 2017. All rights reserved.
 //	Model file generated using JSONExport: https://github.com/Ahmed-Ali/JSONExport
 
+import Realm
 import RealmSwift
 
-class StoryRealm: Object, NSCoding {
+class StoryRealm: Object {
 
 	@objc dynamic var by: String!
-	@objc dynamic var descendants: Int
-	@objc dynamic var id: Int
-    @objc dynamic var kids: List<CommentRealm>!
-	@objc dynamic var score: Int
-	@objc dynamic var time: Int
+	@objc dynamic var descendants: Int = 0
+	@objc dynamic var id: Int = 0
+    var kids = List<CommentRealm>()
+	@objc dynamic var score: Int = 0
+	@objc dynamic var time: Int = 0
 	@objc dynamic var title: String!
 	@objc dynamic var type: String!
 	@objc dynamic var url: String!
 
 
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+
+    func cascadeDelete(realm: Realm) {
+        for comment in kids {
+            comment.cascadeDelete(realm: realm)
+        }
+        realm.delete(kids)
+    }
+
 	/**
 	 * Instantiate the instance using the passed dictionary values to set the properties values
 	 */
-	class func fromDictionary(dictionary: [String:Any]) -> StoryRealm	{
-		let this = StoryRealm()
-		if let byValue = dictionary["by"] as? String{
-			this.by = byValue
-		}
-		if let descendantsValue = dictionary["descendants"] as? Int{
-			this.descendants = descendantsValue
-		}
-		if let idValue = dictionary["id"] as? Int{
-			this.id = idValue
-		}
-		if let kidsArray = dictionary["kids"] as? [[String:Any]]{
-			var kidsItems = List<CommentRealm>()
-			for dic in kidsArray{
-				let value = Int.fromDictionary(dic)
-				kidsItems.addObject(value)
-			}
-			kids = kidsItems
-		}
-		if let scoreValue = dictionary["score"] as? Int{
-			this.score = scoreValue
-		}
-		if let timeValue = dictionary["time"] as? Int{
-			this.time = timeValue
-		}
-		if let titleValue = dictionary["title"] as? String{
-			this.title = titleValue
-		}
-		if let typeValue = dictionary["type"] as? String{
-			this.type = typeValue
-		}
-		if let urlValue = dictionary["url"] as? String{
-			this.url = urlValue
-		}
-		return this
-	}
+    class func fromDictionary(dictionary: [String:Any], realm: Realm) -> StoryRealm {
+        let this = StoryRealm()
+        this.update(fromDictionary: dictionary, realm: realm)
+        return this
+    }
+
+    func update(fromDictionary dictionary: [String: Any], realm: Realm) {
+
+        if kids.count > 0 {
+            cascadeDelete(realm: realm)
+        }
+
+        if let byValue = dictionary["by"] as? String{
+            by = byValue
+        }
+        if let descendantsValue = dictionary["descendants"] as? Int{
+            descendants = descendantsValue
+        }
+        if let idValue = dictionary["id"] as? Int, id == 0 {
+            id = idValue
+        }
+        if let kidsArray = dictionary["kids"] as? [Int]{
+            for idVal in kidsArray {
+                let comment = CommentRealm.fromDictionary(dictionary: ["id" : idVal], realm: realm)
+                kids.append(comment)
+            }
+        }
+        if let scoreValue = dictionary["score"] as? Int{
+            score = scoreValue
+        }
+        if let timeValue = dictionary["time"] as? Int{
+            time = timeValue
+        }
+        if let titleValue = dictionary["title"] as? String{
+            title = titleValue
+        }
+        if let typeValue = dictionary["type"] as? String{
+            type = typeValue
+        }
+        if let urlValue = dictionary["url"] as? String{
+            url = urlValue
+        }
+    }
 
 	/**
 	 * Returns all the available property values in the form of [String:Any] object where the key is the approperiate json key and the value is the value of the corresponding property
@@ -71,14 +90,13 @@ class StoryRealm: Object, NSCoding {
 		}
 		dictionary["descendants"] = descendants
 		dictionary["id"] = id
-		if kids != nil{
-			var dictionaryElements = [[String:Any]]()
+		if kids.count > 0 {
+			var arrayElements = [Int]()
 			for i in 0 ..< kids.count {
-				if let kidsElement = kids[i] as? Int{
-					dictionaryElements.append(kidsElement.toDictionary())
-				}
+                let kidsElement = kids[i]
+                arrayElements.append(kidsElement.id)
 			}
-			dictionary["kids"] = dictionaryElements
+			dictionary["kids"] = arrayElements
 		}
 		dictionary["score"] = score
 		dictionary["time"] = time
@@ -93,51 +111,4 @@ class StoryRealm: Object, NSCoding {
 		}
 		return dictionary
 	}
-
-    /**
-    * NSCoding required initializer.
-    * Fills the data from the passed decoder
-    */
-    @objc required init(coder aDecoder: NSCoder)
-	{
-         by = aDecoder.decodeObject(forKey: "by") as? String
-         descendants = aDecoder.decodeObject(forKey: "descendants") as? Int
-         id = aDecoder.decodeObject(forKey: "id") as? Int
-         kids = aDecoder.decodeObject(forKey: "kids") as? List
-         score = aDecoder.decodeObject(forKey: "score") as? Int
-         time = aDecoder.decodeObject(forKey: "time") as? Int
-         title = aDecoder.decodeObject(forKey: "title") as? String
-         type = aDecoder.decodeObject(forKey: "type") as? String
-         url = aDecoder.decodeObject(forKey: "url") as? String
-
-	}
-
-    /**
-    * NSCoding required method.
-    * Encodes mode properties into the decoder
-    */
-    func encode(with aCoder: NSCoder)
-	{
-		if by != nil{
-			aCoder.encode(by, forKey: "by")
-		}
-         descendants = aDecoder.decodeObject(forKey: "descendants") as? Int
-         id = aDecoder.decodeObject(forKey: "id") as? Int
-		if kids != nil{
-			aCoder.encode(kids, forKey: "kids")
-		}
-         score = aDecoder.decodeObject(forKey: "score") as? Int
-         time = aDecoder.decodeObject(forKey: "time") as? Int
-		if title != nil{
-			aCoder.encode(title, forKey: "title")
-		}
-		if type != nil{
-			aCoder.encode(type, forKey: "type")
-		}
-		if url != nil{
-			aCoder.encode(url, forKey: "url")
-		}
-
-	}
-
 }
