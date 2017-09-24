c7e1b56e7b69c2d062990f560bf8f2e28921b2b2
Vidhyadharan Mohanram <Vidhya_dharan@iCloud.com>
2017-09-25 02:02:53 +0530

    Added Notification related frameworks and code

--- HackerNewsApp/AppDelegate.swift
+++ HackerNewsApp/AppDelegate.swift
@@ -7,8 +7,10 @@
 //
 
 import UIKit
+import UserNotifications
 import FirebaseCore
 import FirebaseAuth
+import FirebaseMessaging
 import GoogleSignIn
 import RealmSwift
 
@@ -19,12 +21,30 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        FirebaseApp.configure()
 
         GIDSignIn.sharedInstance().clientID = FirebaseApp.app()?.options.clientID
         GIDSignIn.sharedInstance().delegate = self
 
         print("Realm URL: \(String(describing: Realm.Configuration.defaultConfiguration.fileURL))")
+
+        if #available(iOS 10.0, *) {
+            // For iOS 10 display notification (sent via APNS)
+            UNUserNotificationCenter.current().delegate = self
+            let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+            UNUserNotificationCenter.current().requestAuthorization(
+                options: authOptions,
+                completionHandler: {_, _ in })
+            // For iOS 10 data message (sent via FCM
+            Messaging.messaging().delegate = self
+        } else {
+            let settings: UIUserNotificationSettings =
+                UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+            application.registerUserNotificationSettings(settings)
+        }
+
+        application.registerForRemoteNotifications()
+
+        FirebaseApp.configure()
 
         manageRealmMigration()
         return true
@@ -97,5 +117,15 @@ extension AppDelegate {
         Realm.Configuration.defaultConfiguration = config
     }
 
+}
+
+extension AppDelegate: UNUserNotificationCenterDelegate, MessagingDelegate {
+    func messaging(_ messaging: Messaging, didRefreshRegistrationToken fcmToken: String) {
+
+    }
+
+    func application(received remoteMessage: MessagingRemoteMessage) {
+        print(remoteMessage.appData)
+    }
 }
 
